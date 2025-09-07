82c6772ec23509521369545c955da1f32c71ab4b
Mohit <mohitkumar24022006@gmail.com>
2025-09-07 14:33:31 +0530

    To Do List Updated

--- index.js
+++ index.js
@@ -44,8 +44,24 @@ document.querySelectorAll(".btn").forEach(btn=>{
 })
 
 
+// TO DO JS
+
+function getHTML(text){
+return `<div class="card">
+                            <div class="left">
+                                <input type="checkbox" class="tick">
+                                <p>${text}</p>
+                            </div>
+                            
+                            <button class="cross">X</button>
+                        </div>`
+}
+
+let tasks=getTasks();
+render();
+
 let add= document.querySelector(".add");
-add.addEventListener("click",()=>{
+    add.addEventListener("click",()=>{
     let text=document.querySelector("#text");
     if(text.value==""){
         let hidden= document.querySelector(".hidden");
@@ -55,15 +71,60 @@ add.addEventListener("click",()=>{
         }, 2000);
     }
     else{
-        let html= `<div class="card">
-                            <div class="left">
-                                <input type="checkbox" id="tick">
-                                <p>${text.value}</p>
-                            </div>
-                            
-                            <button class="cross">X</button>
-                        </div>`
-        document.querySelector(".tasks").innerHTML+= html;
+        tasks.push({text:text.value,done:false});
+        saveTasks(tasks);
+        render();
         text.value="";
     }
-})
+    });
+
+
+
+function saveTasks(tasks){
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+}
+
+function getTasks(){
+    let stored= localStorage.getItem("tasks");
+    try{
+        return stored?JSON.parse(stored):[];
+    }
+    catch(e){
+        localStorage.removeItem("tasks");
+        return [];
+    }
+}
+
+function render(){
+    let block= document.querySelector(".tasks");
+    block.innerHTML="";
+    tasks.forEach((t,e)=>{
+        let html=getHTML(t.text);
+        block.innerHTML+=html;
+    })
+
+    document.querySelectorAll(".tick").forEach((chk,i)=>{
+        chk.checked=tasks[i].done;
+        chk.addEventListener("change",()=>{
+            tasks[i].done=chk.checked;
+            saveTasks(tasks);
+        });
+    });
+
+    document.querySelectorAll(".cross").forEach((btn,i)=>{
+        btn.addEventListener("click",()=>{
+            tasks.splice(i,1);
+            saveTasks(tasks);
+            render();
+        });
+    });
+
+    document.querySelectorAll(".left p").forEach((object,i)=>{
+        object.addEventListener("click",()=>{
+            tasks[i].done= !tasks[i].done;
+            saveTasks(tasks);
+            render();
+        });
+    });
+}
+
